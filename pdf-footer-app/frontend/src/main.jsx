36aadf8821e5a69a6951be31fa9d2dc7bbfa6ab2
TusharSharma <rakeshsharmadj1008@gmail.com>
2026-04-16 01:11:15 +0530

    changes

--- pdf-footer-app/frontend/src/main.jsx
+++ pdf-footer-app/frontend/src/main.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <App />
+    <Analytics />
+  </React.StrictMode>
 )
